504e80d14168aa6f80371fe3fee4eb180e057820
Hrithik Naha <naha.hrithkk@gmail.com>
2021-03-11 18:17:23 +0530

    Add method override to app file and add put and delete routes for blogs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //consting packages
 const express = require("express");
+const methodOverride = require("method-override");
 
 const app = express();
 
 require("./configs/db.js");
 
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 //Routes Calling
-app.use("/", require("./routes/index"));
+app.use("/api", require("./routes/index"));
 
 //Server Listening
 const PORT = process.env.PORT || 5000;
--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,35 @@ const router = express.Router();
 
 router.get("/blogs", (req, res) => {
   Blogs.find({}, (err, blogs) => {
-    if (err) return console.log(`Error while fetching all blog post : ${err}`);
-    if (blogs.length === 0) return res.json("No Blog Post found");
+    if (err)
+      return res.json({ err: `Error while fetching all blog post : ${err}` });
+    if (blogs.length === 0)
+      return res.json({ msg: "No Blog Post found" }).status(200);
     return res.json(blogs).status(200);
   });
 });
 
-router.post("/blog/new", (req, res) => {
+router.post("/blogs", (req, res) => {
   const blog = new Blogs(req.body);
   blog.save((err, blog) => {
-    if (err) return console.log(`Error while saving blog post : ${err}`);
+    if (err) return res.json({ err: `Error while saving blog post : ${err}` });
     return res.json(blog).status(200);
+  });
+});
+
+router.put("/blog/:id", (req, res) => {
+  Blogs.findByIdAndUpdate(req.params.id, req.body, (err, updatedBlog) => {
+    if (err)
+      return res.json({ err: `Error while updating blog post : ${err}` });
+    return res.json(updatedBlog).status(200);
+  });
+});
+
+router.delete("/blog/:id", (req, res) => {
+  Blogs.findByIdAndDelete(req.params.id, (err, deletedBlog) => {
+    if (err)
+      return res.json({ err: `Error while delating blog post : ${err}` });
+    return res.json(deletedBlog).status(200);
   });
 });
 
