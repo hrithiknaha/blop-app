b4fa5b3dddd2453e46d0053fb771c4d2e277308e
Hrithik Naha <naha.hrithkk@gmail.com>
2021-03-15 23:48:08 +0530

    add chai for no reason

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const app = require("./app");
 //Server Listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+module.exports = app;
