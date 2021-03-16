92527efe0342c635a789bb0efe7b9ca6a8f91a45
Hrithik Naha <naha.hrithkk@gmail.com>
2021-03-16 12:36:36 +0530

    add mongoose conection in test files

--- test/Index.test.js
+++ test/Index.test.js
@@ -1,5 +1,18 @@
 const app = require("../app");
 const request = require("supertest");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//Use local db and not cloud for testing
+dotenv.config();
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
 
 test("HTTP test", async () => {
   await request(app).get("/api").expect(200);
